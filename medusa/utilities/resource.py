4b49b71fe0bb2336f1302630c388e5ad38feac26
SpiritualXTC <unforgiven2009@gmail.com>
2023-09-24 13:39:48 +1000

    Update resources to use pathlib

--- medusa/utilities/resource.py
+++ medusa/utilities/resource.py
@@ -3,17 +3,18 @@ from __future__ import annotations
 from types import ModuleType
 
 import pkg_resources
+from pathlib import Path
 
 
 class Resource(object):
     def __init__(self, package: ModuleType, module: str):
-        self._package: ModuleType = package
-        self._module: str = module
+        self.__package: ModuleType = package
+        self.__module: str = module
+        self.__filepath: Path = Path(pkg_resources.resource_filename(self.__package.__name__, self.__module))
 
-    def filename(self) -> str:
-        return pkg_resources.resource_filename(self._package.__name__, self._module)
+    @property
+    def filepath(self) -> Path:
+        return self.__filepath
 
     def read(self, binary: bool = False) -> str | bytes:
-        filename = pkg_resources.resource_filename(self._package.__name__, self._module)
-
-        return open(filename).read()
+        return open(self.filepath, "rb" if binary else "r").read()
