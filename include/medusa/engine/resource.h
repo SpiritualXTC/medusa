a0898e098832917d71adb4f8477b8f62901c1f23
SpiritualXTC <unforgiven2009@gmail.com>
2025-09-23 23:18:06 +1000

    Convert tabs -> spaces

--- include/medusa/engine/resource.h
+++ include/medusa/engine/resource.h
@@ -4,25 +4,25 @@
 
 namespace medusa
 {
-	template <typename T>
-	class ResourceID
-	{
-	private:
-		static uint64_t generate_id()
-		{
-			static uint64_t AUTO_ID = 0;
+    template <typename T>
+    class ResourceID
+    {
+    private:
+        static uint64_t generate_id()
+        {
+            static uint64_t AUTO_ID = 0;
 
-			return ++AUTO_ID;
-		}
+            return ++AUTO_ID;
+        }
 
-	public:
-		ResourceID() {}
-		virtual ~ResourceID() {}
+    public:
+        ResourceID() {}
+        virtual ~ResourceID() {}
 
-		inline const uint64_t id() const { return _id; }
+        inline const uint64_t id() const { return _id; }
 
-	private:
-		uint64_t _id = ResourceID::generate_id();
-	};
+    private:
+        uint64_t _id = ResourceID::generate_id();
+    };
 
 }
