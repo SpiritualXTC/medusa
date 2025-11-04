cb325b5419d8b88e3c1e4c62119badf5e734e1c8
SpiritualXTC <unforgiven2009@gmail.com>
2025-11-05 10:56:32 +1100

    Remove redundant RenderableList

--- include/medusa/renderer/renderable.h
+++ include/medusa/renderer/renderable.h
@@ -56,41 +56,4 @@ namespace medusa
         /// <returns></returns>
         virtual bool render(std::shared_ptr<IPass> pass) = 0;
     };
-
-
-    /// <summary>
-    /// Naive RenderableList, render the iterable directly
-    /// </summary>
-    class RenderableList : public IRenderableContainer
-    {
-    public:
-        RenderableList(){}
-        virtual ~RenderableList() {}
-
-        bool add(std::shared_ptr<IRenderable> renderable)
-        {
-            _list.push_back(renderable);
-
-            return true;
-        }
-
-        bool clear()
-        {
-            _list.clear();
-
-            return true;
-        }
-
-        bool render(std::shared_ptr<IPass> pass)
-        {
-            for (auto& r : _list)
-                r->render(pass);
-
-            return true;
-        }
-
-
-    private:
-        std::list<std::shared_ptr<IRenderable>> _list;
-    };
 }
