f107ca04c54bc077fff0eaff398dce2187945214
SpiritualXTC <unforgiven2009@gmail.com>
2025-11-05 11:10:23 +1100

    Add basic SceneRenderer

---
+++ src/engine/game/scene_renderer.cpp
@@ -0,0 +1,85 @@
+#include "scene_renderer.h"
+
+#include <medusa/engine/context.h>
+#include <medusa/graphics.h>
+
+#include <core/utilities/logging.h>
+
+
+
+using namespace medusa;
+
+
+//
+MeshContainer::MeshContainer(std::shared_ptr<IContext> context, std::shared_ptr<GenericArray<glm::mat4>> transformBuffer)
+    : _context(context)
+    , _transformBuffer(transformBuffer)
+{
+
+}
+
+
+//
+MeshContainer::~MeshContainer()
+{
+
+
+}
+
+
+//
+std::shared_ptr<ModelComponent> MeshContainer::createInstance(std::shared_ptr<IMesh> mesh)
+{
+    auto transformBuffer = _transformBuffer.lock();
+    auto context = _context.lock();
+
+    // RAII: Create the Model Instances
+    std::shared_ptr<ModelInstance> instance = nullptr;
+
+    auto it = _instances.find(mesh->id());
+    if (it == _instances.end())
+    {
+        // Add new instance
+        instance = std::make_shared<ModelInstance>(context, mesh, transformBuffer);
+        _instances.insert({ mesh->id(), instance });
+    }
+    else
+    {
+        instance = it->second;
+    }
+
+    std::shared_ptr<ModelComponent> component = std::make_shared<ModelComponent>(instance, transformBuffer);
+
+    return component;
+}
+
+
+//
+bool MeshContainer::render(std::shared_ptr<IPass> pass)
+{
+    if (pass != nullptr)
+        throw MedusaError("Deprecated");
+
+    //logging::info("Rendering mesh containers");
+
+    for (auto instance : _instances)
+        instance.second->render();
+
+    return true;
+}
+
+
+
+
+//
+SceneRenderer::SceneRenderer()
+{
+
+}
+
+
+//
+SceneRenderer::~SceneRenderer()
+{
+
+}
---
+++ src/engine/game/scene_renderer.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <medusa/medusa.h>
+#include <medusa/engine/resource.h>
+#include <medusa/graphics_fwd.h>
+
+#include <medusa/renderer/renderable.h>
+
+#include <engine/graphics/model_instance.h>
+#include <engine/game/components/model_component.h>
+
+namespace medusa
+{
+    // TODO: Could this just be RenderableMap ?
+
+    // TODO: Needs an interface for rendering -- NOT for creating instance?
+    //      Could probably just reuse the container? -- only
+    // TODO: Temp Interface [rename]
+    class MeshContainer : public IRenderableContainer
+    {
+    public:
+        MeshContainer(std::shared_ptr<IContext> context, std::shared_ptr<GenericArray<glm::mat4>> transformBuffer);
+        virtual ~MeshContainer();
+
+        std::shared_ptr<ModelComponent> createInstance(std::shared_ptr<IMesh> mesh);
+
+
+
+
+        /// <summary>
+        /// Add an item to the iterable
+        /// </summary>
+        /// <param name="renderable"></param>
+        /// <returns></returns>
+        bool add(std::shared_ptr<IRenderable> renderable) override { throw MedusaError("Deprecated"); }
+
+
+        /// <summary>
+        /// Clear the iterable
+        /// </summary>
+        /// <returns></returns>
+        bool clear() override { throw MedusaError("Deprecated"); }
+
+
+        /// <summary>
+        /// Render all the items in the iterable
+        /// </summary>
+        /// <returns></returns>
+        bool render(std::shared_ptr<IPass> pass) override;
+    private:
+
+        std::weak_ptr<IContext> _context;
+        std::weak_ptr<GenericArray<glm::mat4>> _transformBuffer;
+
+        std::unordered_map<uint64_t, std::shared_ptr<ModelInstance>> _instances;
+
+    };
+
+
+
+
+    // TODO: This will be moved to the SceneGraph as part of the update process
+    class SceneRenderer
+    {
+    public:
+        SceneRenderer();
+        virtual ~SceneRenderer();
+
+        // TODO: Add Renderable? -- Only RenderableContainer IS an IRenderable?
+        bool addContainer(std::shared_ptr<IRenderableContainer> container)
+        {
+            _sceneContainers.push_back(container);
+            return true;
+        }
+
+
+    private:
+        std::vector<std::shared_ptr<IRenderableContainer>> _sceneContainers;
+
+    };
+
+}
