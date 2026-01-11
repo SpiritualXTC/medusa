2e8708894aa9f0187061aa0742c12d773991fb63
SpiritualXTC <unforgiven2009@gmail.com>
2026-01-11 23:01:03 +1100

    Remove confusion with IDescriptor::render() and the optional instances parameter

--- include/medusa/graphics/descriptor.h
+++ include/medusa/graphics/descriptor.h
@@ -25,7 +25,6 @@ namespace medusa
         virtual bool bind() = 0;
         virtual bool unbind() = 0;
 
-        virtual bool render(PrimitiveType primitiveType, size_t vertices, size_t indices = 0) = 0;
         virtual bool render(PrimitiveType primitiveType, size_t vertices, size_t indices = 0, size_t instances = 1) = 0;
 
         virtual bool renderIndirect(PrimitiveType primitiveType, std::shared_ptr<GenericArray<Indirect>> indirect) = 0;
