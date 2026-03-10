5554ee9d43ec6e2d6593390a23b0c77771d8a15e
SpiritualXTC <unforgiven2009@gmail.com>
2026-03-10 22:49:55 +1100

    Initialise the SDL Image library

--- src/opengl/context_gl.cpp
+++ src/opengl/context_gl.cpp
@@ -5,6 +5,7 @@
 #include <string>
 
 #include <SDL.h>
+#include <SDL_image.h>
 
 #include <medusa/exception.h>
 
@@ -40,7 +41,6 @@ ContextGL::ContextGL(std::shared_ptr<Config> config)
         throw MedusaError("Unable to Initialise SDL");
     logging::info("SDL Initialised");
 
-
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
@@ -48,6 +48,9 @@ ContextGL::ContextGL(std::shared_ptr<Config> config)
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+    if (IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG) == 0)
+        throw MedusaError("Unable to Initialise SDLImage");
 
     // Create the Window
     int w = config->getValue<int>("medusa.window.width", 800);
