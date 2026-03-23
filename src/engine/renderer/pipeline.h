8ce908d9aa66da7c62a9c4c3b635b5eea1838a80
SpiritualXTC <unforgiven2009@gmail.com>
2026-03-24 00:57:22 +1100

    Remove commented struct

--- src/engine/renderer/pipeline.h
+++ src/engine/renderer/pipeline.h
@@ -13,15 +13,6 @@ namespace medusa
 
     struct Indirect;
 
-    //struct MeshIndirect
-    //{
-    //    uint32_t count;
-    //    uint32_t instanceCount;
-    //    uint32_t firstIndex;
-    //    uint32_t baseVertex;
-    //    uint32_t baseInstance;
-    //};
-
 
     // Generic Pipeline
     class Pipeline : public IPipeline
