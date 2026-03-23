3461bfec65404b89a597ee9bd1e8a44837118e18
SpiritualXTC <unforgiven2009@gmail.com>
2026-03-24 00:59:53 +1100

    Add the missing IEngine interface (oops)

---
+++ include/medusa/engine/engine.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <medusa/medusa.h>
+
+#include <medusa/engine_fwd.h>
+
+namespace medusa
+{
+    class IApplication;
+
+    class IEngine
+    {
+    public:
+        IEngine() {}
+        virtual ~IEngine() {}
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="application"></param>
+        virtual void run(std::shared_ptr<IApplication> application) = 0;
+
+
+        /// <summary>
+        ///
+        /// </summary>
+        virtual void quit() = 0;
+
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
+        virtual const inline std::shared_ptr<IWindow>& window() = 0;
+
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
+        virtual const inline std::shared_ptr<IContext>& context() = 0;
+
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
+        virtual const inline std::shared_ptr<Timing>& timing() = 0;
+    };
+
+}
